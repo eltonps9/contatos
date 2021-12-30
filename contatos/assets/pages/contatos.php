0df2ab1f0f0b20eff882de6a1cd00291bf44c141
Elton Passos <eltonps19@gmail.com>
2021-12-30 17:15:20 -0300

    mudanca no css e adicionando tela de detalhes dos contatos

--- contatos/assets/pages/contatos.php
+++ contatos/assets/pages/contatos.php
@@ -1,6 +1,9 @@
 <?php
 require "../config/conexao.php";
-
+if($_SESSION['login']==false){
+    header('Location:../../index.html');
+    exit;
+}
 $contact=[];
 
 $sql=$pdo->query("SELECT * FROM contatos");
@@ -24,54 +27,70 @@ if($sql->rowCount()>0){
 
 <body>
     
+        
+    
     <main>
         
         <div id="login">
             <div id="foto"></div>
-            <p><?php
-            session_start();
-            if($_SESSION['login']){
-                echo $_SESSION['login'];
+            <div class="para">
+                <p>
+                    <?php
+                        session_start();
+                        if($_SESSION['login']){
+                            echo $_SESSION['login'];
 
-            }
-            ?>
-            </p>
+                        }
+                    ?>
+                </p>
+            </div>
         </div>
     
             
-        <div id="opicao">
         
-            <nav class="nav">
-            
-                <a href="" id="mostrar" >
-                    <div >
+        
+        <nav class="nav">
+        
+            <div href="" id="mostrar" onclick="trocar_usu()">
+                <div class="mostrar" >
+                   <a href="../config/sair_login.php">
                         <div class="menu1"></div>
                         <div class="menu1"></div>
                         <div class="menu1"></div>
-                    </div>
-                </a>
-                <div id="mais">
-                    <a href="../config/cadastros.php">+</a>
+                   </a>
                 </div>
-            </nav>
-            
+            </div>
+            <div id="mais">
+                <a href="../config/cadastros.php">+</a>
+            </div>
+        </nav>
+        <div id="contatos-area">
+
             <?php foreach($contact as $listas):?>
-                
                 <div id="contato">
-
-                    <a" id="fotocontato"></a>
-
+                    <div class="fto"></div>
+                    
                     <p><?=$listas['nome'];?></p>
+                    
                     <div id="menu">
-                        <a href="./detals.php?id=<?=$listas['id'];?>" id="edita"> DETALHES</a>
-                        <a href="../config/editar.php?id=<?=$listas['id'];?>" id="edita">EDITAR</a>
-                        <a href="../config/remove.php?id=<?=$listas['id'];?>" id="edita">EXCLUIR</a>
+                        <div id="edita"><a href="./detals.php?id=<?=$listas['id'];?>">Detalhes</a></div>
+                        <div id="edita"><a href="../config/remove.php?id=<?=$listas['id'];?>">Excluir</a></div>
                     </div>
                 </div>
             <?php endforeach;?>
             
         </div> 
     </main>
+    <aside id="tela">
+        
+        <a href="trocar.php">Trocar usuario</a>
+        
+        <p>tudo o que voce precisa é clicar ai</p>
+        <div class="img"><img src="" alt=""></div>
+        <div class="sr" onclick="sair()">
+            <p>+</p>
+        </div>
+    </aside>
 </body>
 <script src="../js/script_login.js"></script>
 
